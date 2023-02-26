de78aa2e8174abe447ab20cc2ded578f1dae64a0
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2023-02-26 22:04:18 +0100

    [core] Update PIO builders to new structure

--- builder/utils/env.py
+++ builder/utils/env.py
@@ -1,13 +1,16 @@
 # Copyright (c) Kuba Szczodrzyński 2022-05-04.
 
+import json
 import sys
 from os.path import isdir, join
 from subprocess import PIPE, Popen
 
 from ltchiptool import Family
-from SCons.Script import DefaultEnvironment
+from platformio.platform.base import PlatformBase
+from platformio.platform.board import PlatformBoardConfig
+from SCons.Script import DefaultEnvironment, Environment
 
-env = DefaultEnvironment()
+env: Environment = DefaultEnvironment()
 
 
 def read_version(platform_dir: str, version: str):
@@ -47,33 +50,27 @@ def read_version(platform_dir: str, version: str):
     return f"{version}+{build_str}" if build_str else version
 
 
-def env_add_defaults(env, platform, board):
+def env_configure(env: Environment, platform: PlatformBase, board: PlatformBoardConfig):
+    # Read external libraries list
+    with open(join(platform.get_dir(), "external-libs.json")) as f:
+        external_libs = json.load(f)
     # Get Family object for this board
     family = Family.get(short_name=board.get("build.family"))
     # Default environment variables
     vars = dict(
-        SDK_DIR=platform.get_package_dir(family.framework),
+        SDK_DIR=platform.get_package_dir(board.get("package")),
         LT_DIR=platform.get_dir(),
-        # Root dirs
+        CORES_DIR=join("${LT_DIR}", "cores"),
+        COMMON_DIR=join("${LT_DIR}", "cores", "common"),
+        # Build directories & paths
         BOARD_DIR=join("${LT_DIR}", "boards", "${VARIANT}"),
-        ARDUINO_DIRS={
-            "$FAMILY_CODE": join("${LT_DIR}", "arduino", "${FAMILY_NAME}"),
-            "$FAMILY_PARENT_CODE": join("${LT_DIR}", "arduino", "${FAMILY_PARENT}"),
-        },
-        FAMILY_DIR=join("${LT_DIR}", "platform", "${FAMILY_NAME}"),
-        PARENT_DIR=join("${LT_DIR}", "platform", "${FAMILY_PARENT}"),
-        TOOLS_DIR=join("${LT_DIR}", "tools"),
-        # Family-specific paths
-        BIN_DIR=join("${FAMILY_DIR}", "bin"),
-        OPENOCD_DIR=join("${FAMILY_DIR}", "openocd"),
-        # Use SDK linker script by default
-        LDSCRIPT_PATH=["${LDSCRIPT_SDK}"],
+        FAMILY_DIR=join("${LT_DIR}", "cores", "${FAMILY_NAME}"),
+        MISC_DIR=join("${FAMILY_DIR}", "misc"),
+        LDSCRIPT_PATH=[board.get("build.ldscript")],
         # Board config variables
         MCU=board.get("build.mcu").upper(),
-        MCULC=board.get("build.mcu"),
+        MCULC=board.get("build.mcu").lower(),
         VARIANT=board.get("build.variant"),
-        LDSCRIPT_SDK=board.get("build.ldscript_sdk"),
-        LDSCRIPT_ARDUINO=board.get("build.ldscript_arduino"),
         # ltchiptool config:
         # -r    output raw log messages
         # -i 1  indent log messages
@@ -81,32 +78,17 @@ def env_add_defaults(env, platform, board):
         # Fix for link2bin to get tmpfile name in argv
         LINKCOM="${LINK} ${LINKARGS}",
         LINKARGS="${TEMPFILE('-o $TARGET $LINKFLAGS $__RPATH $SOURCES $_LIBDIRFLAGS $_LIBFLAGS', '$LINKCOMSTR')}",
+        # Store the family object
+        FAMILY_OBJ=family,
+        EXTERNAL_LIBS=external_libs,
     )
     env.Replace(**vars)
     # Store family parameters as environment variables
     env.Replace(**dict(family))
     # Default build options
     env.Prepend(
-        CPPPATH=[
-            "$LT_DIR/platform/common/config",
-            "$LT_DIR/platform/common/fixups",
-            "$LT_DIR/platform/common/fixups/lib_inc",
-            "$BOARD_DIR",
-            "$FAMILY_DIR/config",
-            "$PARENT_DIR/config",
-            "$FAMILY_DIR/fixups",
-            "$PARENT_DIR/fixups",
-            "$FAMILY_DIR/fixups/inc",
-            "$PARENT_DIR/fixups/inc",
-        ],
-        LIBPATH=[
-            "$FAMILY_DIR/ld",
-            "$PARENT_DIR/ld",
-            "$FAMILY_DIR/fixups",
-            "$PARENT_DIR/fixups",
-        ],
         CPPDEFINES=[
-            ("LIBRETUYA", "1"),
+            ("LIBRETUYA", 1),
             ("LT_VERSION", read_version(platform.get_dir(), platform.version)),
             ("LT_BOARD", "${VARIANT}"),
             ("F_CPU", board.get("build.f_cpu")),
@@ -119,4 +101,4 @@ def env_add_defaults(env, platform, board):
     )
 
 
-env.AddMethod(env_add_defaults, "AddDefaults")
+env.AddMethod(env_configure, "ConfigureEnvironment")
